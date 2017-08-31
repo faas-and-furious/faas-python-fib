12e855e071d2d6ceba91ef90166ac296166f23c5
Finnian Anderson <get@finnian.io>
2017-08-31 21:32:25 +0100

    Fix handler function

--- handler/handler.py
+++ handler/handler.py
@@ -4,10 +4,11 @@ def fib(n):
 	if n <= 1: return n
 	else: return fib(n-1) + fib(n-2)
 
-for line in sys.stdin:
-	n = int(line.strip())
+def handle(req):
+	n = int(req)
 
 	output = []
 	for i in range(n):
 		output.append(str(fib(i)))
+
 	print(', '.join(output))
